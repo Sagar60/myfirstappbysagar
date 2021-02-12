5a14a790967b1a4cefda559566ad4ae32254fd00
Sagar60 <sagardaa058.com>
2021-02-12 16:07:42 +0530

    make it better

---
+++ first/server.js
@@ -0,0 +1,42 @@
+// var http = require('http');
+
+// http.createServer(function (req, res) {
+//   res.writeHead(504, {'Content-Type': 'text/html'});
+//   res.end('Hello World!'+'Welcome to my server');
+// }).listen(8080);
+
+
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/html'))
+
+// app.get('/',(req,res)=>{
+//     res.send('Hello Sagar');
+// })
+app.get('/cal',(req,res)=>{
+    let result = 0;
+    const q1 = parseInt(req.query.q1);
+    const q2 = parseInt(req.query.q2)
+    result = q1+q2;
+    res.send(`
+        <h3> Answer is ${result} </h3>
+    `
+    
+    );
+});
+app.get('/cal/:oper',(req,res)=>{
+    let result = 0;
+    const q1 = parseInt(req.query.q1);
+    const q2 =  parseInt(req.query.q2);
+    result = q1+q2;
+
+    res.send(`${result}`);
+    
+})
+
+
+app.listen(8080,()=>{
+    console.log('Server started at 8080');
+})
+
